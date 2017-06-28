626606871aba1e669a1cd75cf5a6271fe90e28d8
Mikhail Pridushchenko <mikhail.pridushchenko@dsr-company.com>
2017-06-28 16:22:53 +0300

    UNIX services write logs to stdout
    To better adopt SoftEther VPN for execution in Docker container all
    services output logs to stdout if running in foreground.

--- src/Mayaqua/Unix.c
+++ src/Mayaqua/Unix.c
@@ -1650,6 +1650,23 @@ void *UnixFileOpen(char *name, bool write_mode, bool read_lock)
 	return (void *)p;
 }
 
+// Get UNIXIO object for stdout
+void* GetUnixio4Stdout()
+{
+	static UNIXIO unixio =
+	{
+		.fd = -1,
+		.write_mode = true
+	};
+
+	if (g_foreground)
+	{
+		unixio.fd = STDOUT_FILENO;
+		return &unixio;
+	}
+	return NULL;
+}
+
 // Return the current thread ID
 UINT UnixThreadId()
 {
